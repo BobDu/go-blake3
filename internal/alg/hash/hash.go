01bc629713cb03ad09633e035f494b4ef250e71f
BobDu <i@bobdu.cc>
2026-08-21 07:25:10 +0800

    hash two chunk inputs with the avx512 backend
    Signed-off-by: BobDu <i@bobdu.cc>

--- internal/alg/hash/hash.go
+++ internal/alg/hash/hash.go
@@ -10,7 +10,7 @@ import (
 )
 
 func HashF(input *[8192]byte, length, counter uint64, flags uint32, key *[8]uint32, out *[64]uint32, chain *[8]uint32) {
-	if consts.HasAVX512 && length > 2*consts.ChunkLen {
+	if consts.HasAVX512 && length > consts.ChunkLen {
 		hash_avx512.HashF(input, length, counter, flags, key, out, chain)
 	} else if consts.HasAVX2 && length > 2*consts.ChunkLen {
 		hash_avx2.HashF(input, length, counter, flags, key, out, chain)
